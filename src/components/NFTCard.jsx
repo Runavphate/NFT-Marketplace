3007ba49868a57910c18e2c3bf49d1549ff49ea8
Runav Phate <phaterunav@gmail.com>
2025-07-20 13:06:28 +0530

    Update NFTCard.jsx

--- src/components/NFTCard.jsx
+++ src/components/NFTCard.jsx
@@ -1,7 +1,7 @@
 const NFTCard = ({ image, title, price }) => (
     <div className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition" >
-      <img src={image} alt={title} className="w-full h-60 object-cover"/>
-      <div className="p-4">
+      <img src={image} alt={title} className="w-full h-40 object-cover"/>
+      <div className="p-3 text-sm">
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-sm text-gray-600">{price} ETH</p>
       </div>
