6400e66961213197e77653fe2b03b668dd6ca942
Runav Phate <phaterunav@gmail.com>
2025-07-28 19:11:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import WalletConnect from "./WalletConnect";
 
 const Header = () => (
-  <header className="flex justify-between items-center px-6 py-4 bg-gray-100 shadow">
-    <img src="../public/Logo.png"></img>
+  <header className="flex justify-between items-center px-6 py-4 bg-black-100 shadow">
+    <img src="./public/Logo.png"></img>
     <h1 className="text-2xl font-bold">One Market</h1>
     <nav className="flex gap-4">
       <Link to="/" className="hover:underline">Home</Link>
