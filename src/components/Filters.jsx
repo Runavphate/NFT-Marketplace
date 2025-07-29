3cc56f496c596bf993cbda389b732d0072359e50
Runav Phate <phaterunav@gmail.com>
2025-07-29 17:22:06 +0530

    Update Filters.jsx

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,9 @@
 <div className="flex gap-4 flex-wrap">
   <select className="px-4 py-2 border rounded bg-white shadow">
     <option>All Categories</option>
-    <option>Art</option>
+    <option>Apes</option>
     <option>Collectibles</option>
-    <option>Music</option>
+    <option>Space</option>
   </select>
   <select className="px-4 py-2 border rounded bg-white shadow">
     <option>Sort by Price</option>
