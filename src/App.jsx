e2396d76a72c06e40d2443524f75142e9d1744f7
Runav Phate <phaterunav@gmail.com>
2025-07-29 17:25:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import HeroBanner from "../components/HeroBanner";
+import Filters from "../components/Filters";
 import Mint from "./pages/Mint";
 import Explore from "./pages/Explore";
 import Profile from "./pages/Profile";
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/filters" element={<Filters />} />
+        <Route path="/herobanner" element={<HeroBanner />} />
         <Route path="/mint" element={<Mint />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/profile" element={<Profile />} />
