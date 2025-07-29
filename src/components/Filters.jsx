7949e348f855cef28de0097872e92e9c25d7e8d5
Runav Phate <phaterunav@gmail.com>
2025-07-29 17:11:26 +0530

    Create Filters.jsx

---
+++ src/components/Filters.jsx
@@ -0,0 +1,13 @@
+<div className="flex gap-4 flex-wrap">
+  <select className="px-4 py-2 border rounded bg-white shadow">
+    <option>All Categories</option>
+    <option>Art</option>
+    <option>Collectibles</option>
+    <option>Music</option>
+  </select>
+  <select className="px-4 py-2 border rounded bg-white shadow">
+    <option>Sort by Price</option>
+    <option>Lowest First</option>
+    <option>Highest First</option>
+  </select>
+</div>
