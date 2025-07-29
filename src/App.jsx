a42aa19f1a80c4345b229ae3f432f455e6908172
Runav Phate <phaterunav@gmail.com>
2025-07-29 17:48:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HeroBanner from "./components/HeroBanner";
 import Filters from "./components/Filters";
 import Home from "./pages/Home";
 import Mint from "./pages/Mint";
