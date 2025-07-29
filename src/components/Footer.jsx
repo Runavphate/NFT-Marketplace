a9b87fa3ba4620cb51ef0f26fd9ca6d13630b15d
Runav Phate <phaterunav@gmail.com>
2025-07-29 17:47:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => (
     <footer className="text-center py-4 text-sm text-gray-500 bg-gray-50">
-      © {new Date().getFullYear()} NFT Marketplace. All rights reserved.
+      © {new Date().getFullYear()} One Market. All rights reserved.
     </footer>
   );
   
