9373978757fe91d766c708450714ee25bbcba83b
Runav Phate <phaterunav@gmail.com>
2025-08-09 21:44:17 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,7 @@ const Explore = () => {
 
   return (
     <main className="pt-20">
-    <div className="p-6">
+    <div className="bg-black p-6">
       <h2 className="text-xl font-bold mb-4">Explore NFTs</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {listedNFTs.map(nft => <NFTCard key={nft.id} {...nft} />)}
