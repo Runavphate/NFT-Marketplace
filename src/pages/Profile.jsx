7cca6836553d68cb1ed3b8ec2cad69536303923b
Runav Phate <phaterunav@gmail.com>
2025-08-09 21:52:54 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ const Explore = () => {
   const listedNFTs = [ /* fetch from contract or dummy */ ];
 
   return (
-    <main className="pt-[75px]">
+    <main className="pt-[74px]">
     <div className="bg-black p-6">
       <h2 className="text-xl text-white font-bold mb-4">Explore NFTs</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
