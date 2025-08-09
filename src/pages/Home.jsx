40d0aaca5f30ae0711349e26269a1ff732cf825e
Runav Phate <phaterunav@gmail.com>
2025-08-09 21:45:28 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
   ];
   
   return (
-    <main className="pt-20">
+    <main className="pt-19">
     <div className="bg-black p-6">
       <h2 className="text-2xl text-white font-bold mb-4">Trending NFTs</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
