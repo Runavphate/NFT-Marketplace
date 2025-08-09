7ccf472e736fae25058d2de39dc0f0f68b5f69b7
Runav Phate <phaterunav@gmail.com>
2025-08-09 21:47:17 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,9 @@ const Explore = () => {
   const listedNFTs = [ /* fetch from contract or dummy */ ];
 
   return (
-    <main className="pt-20">
+    <main className="pt-19">
     <div className="bg-black p-6">
-      <h2 className="text-xl font-bold mb-4">Explore NFTs</h2>
+      <h2 className="text-xl text-white font-bold mb-4">Explore NFTs</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {listedNFTs.map(nft => <NFTCard key={nft.id} {...nft} />)}
       </div>
