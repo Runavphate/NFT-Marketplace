56698ed65e1053a35781a010687be9baf1816e9f
Runav Phate <phaterunav@gmail.com>
2025-08-09 20:59:12 +0530

    Update Mint.jsx

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -17,7 +17,7 @@ const Mint = () => {
         <input type="text" name="title" placeholder="Title" className="border px-4 py-2 w-full" onChange={handleChange} />
         <input type="url" name="imageUrl" placeholder="Image URL" className="border px-4 py-2 w-full" onChange={handleChange} />
         <input type="text" name="metadataURI" placeholder="Metadata URI" className="border px-4 py-2 w-full" onChange={handleChange} />
-        <button className="bg-green-500 text-white px-6 py-2 rounded">Mint NFT</button>
+        <button className="bg-gray-500 text-white px-6 py-2 hover:bg-black rounded">Mint NFT</button>
       </form>
     </div>
   );
