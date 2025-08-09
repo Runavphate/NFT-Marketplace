8ebd001fcd4d6817beeeb1598902fb7bfd393320
Runav Phate <phaterunav@gmail.com>
2025-08-09 20:55:59 +0530

    Update NFTCard.jsx

--- src/components/NFTCard.jsx
+++ src/components/NFTCard.jsx
@@ -4,7 +4,7 @@ const NFTCard = ({ image, title, price }) => (
   <div className="p-4">
     <h2 className="text-xl font-semibold">{title}</h2>
     <div className="flex items-center justify-between mt-4">
-      <span className="text-indigo-600 font-bold">{price} ETH</span>
+      <span className="text-gray-600 font-bold">{price} ETH</span>
       <button className="text-white bg-gray-500 px-4 py-2 rounded hover:bg-black transition">
         Buy Now
       </button>
