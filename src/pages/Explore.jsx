67f2ea644b60183b76728bfe28d47cb04e84e280
Runav Phate <phaterunav@gmail.com>
2025-08-10 11:11:22 +0530

    Update Explore.jsx

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -139,7 +139,7 @@ const Explore = () => {
   };
 
   return (
-   <main className="pt-[75px]">
+   <main className="pt-[72px]">
     <div className="bg-black p-6">
       <h2 className="text-2xl text-white font-bold mb-2">Explore NFTs</h2>
 
